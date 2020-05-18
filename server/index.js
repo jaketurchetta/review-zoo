6dda0456122aaf8a4b12e969cd5a4317e749c106
Jake Turchetta <14jturchetta@gmail.com>
2020-05-18 10:06:44 -0400

    updated express static reference

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
-app.use( express.static(__dirname + '/../client/dist') );
+app.use( express.static(__dirname + '/client/dist') );
 
 // GET REVIEWS
 app.get('/products/:productid/topreviews', (req, res) => {
